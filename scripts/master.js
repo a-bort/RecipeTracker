8ea0a73758d4e8b0fc050f3a72b17b60dcbfdfde
a-bort <bortron5000@gmail.com>
2014-02-09 19:02:40 -0500

    Refactor Project to use Express

--- scripts/master.js
+++
@@ -1,3 +0,0 @@
-$.getScript("scripts/util.js", function(){ util.log("loaded util"); });
-$.getScript("scripts/lib/bootstrap/bootstrap.js", function(){ util.log("loaded bootstrap");});
-$.getScript("scripts/lib/angular/angular.js", function(){ util.log("loaded angular");});
