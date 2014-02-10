4bee447f477380647a86ab2cffbd4616fdc5cebe
a-bort <bortron5000@gmail.com>
2014-02-09 22:12:49 -0500

    Use a layout page

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,21 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'To Do List', todos: [
+    { 
+        description: "Buy Groceries",
+        due : new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+        done: false
+    },
+    { 
+        description: "Do Laundry",
+        due : new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
+        done: false
+    },
+    { 
+        description: "Rescue puppies from burning building",
+        due : new Date(new Date().getTime() + 4 * 24 * 60 * 60 * 1000),
+        done: true
+    }    
+  ] });
 };
